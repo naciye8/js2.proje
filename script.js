9dbeadd361c2e30e5dd7d87122ca8135e14c6706
naciye <naciye.sari.koyuncu@gmail.com>
2021-12-10 21:40:16 -0300

    jsproje2

---
+++ script.js
@@ -0,0 +1,12 @@
+const myAge=27;
+//simdiki yasim
+let earlyYears=2;
+//ilk 2 yil 10.5 ile carpilir.
+earlyYears=earlyYears*10.5;
+let myearlyYears=10.5*earlyYears;
+let laterYears=myAge-2;
+let mylaterYears=4*laterYears;
+console.log(myearlyYears);
+console.log(mylaterYears);
+let myDogYears=myearlyYears+mylaterYears;
+console.log(myDogYears);
